a5298494129457c57f3655f3957d0329fb289220
luren <byljcron@gmail.com>
2018-03-28 21:55:58 -0500

    update

--- hw52a.py
+++ hw52a.py
@@ -27,17 +27,26 @@ def mle(n):
     else:
         return False
 
-
+def uncertainty(n,x):
+    global result
+    dab=0
+    for i in range(0,n):
+        dab=dab+(2* (-0.25 + (result[i] - x)**2))/(0.25 + (result[i] - x)**2)**2
+    return -1.0/dab
 
 
 result=[RnC()]
 for a in range(0,1000):
     result.append(RnC())
 for i in range(1,8):
-    print(2**i,mle(2**i))
-print(500,mle(500))
-print(1000,mle(1000))
-plt.hist(result, bins='auto')
+    print(2**i,mle(2**i),uncertainty(2**i,mle(2**i)))
+print(500,mle(500),uncertainty(500,mle(500)))
+print(1000,mle(1000),uncertainty(1000,mle(1000)))
+#plt.hist(result, bins='auto')
+d = np.arange(0.0, 5.0, 0.01)
+plt.plot(d,func(d))
+plt.axvline(x=uncertainty(64,mle(64))+0.8, color='r', linestyle='-')
+plt.axvline(x=uncertainty(1000,mle(1000))+0.8, color='b', linestyle='-')
 plt.show()
 
 
